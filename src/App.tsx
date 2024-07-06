2054c4062460e7e27a7d18025fbfb6bb45b07d20
keyaru77 <whyudacok@gmail.com>
2024-07-06 09:36:11 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { Route, BrowserRouter } from 'react-router-dom'
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 
 import './styles/global.scss'
